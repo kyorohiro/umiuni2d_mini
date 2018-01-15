6594d17d66bfa0e42880e70dc0004931e283a743
kyorohiro <kyorohiro@gmail.com>
2018-01-15 10:00:29 +0900

    ..

--- engine_core/lib/core.dart
+++ engine_core/lib/core.dart
@@ -17,6 +17,7 @@ part 'core/rect.dart';
 part 'core/size.dart';
 part 'core/sprite.dart';
 part 'core/stage.dart';
+part 'core/stage_base.dart';
 part 'core/displayobject_expansion.dart';
 part 'core/displayobject_expansion_item.dart';
 part 'core/builder.dart';
