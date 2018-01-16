b8755eda92bba1111ea8b455a54b113e25d3c08e
kyorohiro <kyorohiro@gmail.com>
2018-01-16 09:46:23 +0900

    mod kick start code

--- example/draw_primitive/web/main.dart
+++ example/draw_primitive/web/main.dart
@@ -8,7 +8,6 @@ import 'package:umiuni2d_webgl/core.dart';
 import 'package:example/primitive_test.dart';
 
 void main() {
-  TinyGameBuilderForWebgl builder = new TinyGameBuilderForWebgl();
   GameWidget game = new GameWidget();
   game.stage.root.addChild(new PrimitiveTest());
   game.start();
