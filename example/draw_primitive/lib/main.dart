4fa21274681effca28e4bc52a98bb77cfc3ac4bc
kyorohiro <kyorohiro@gmail.com>
2018-01-16 09:14:35 +0900

    add GameWidget

--- example/draw_primitive/lib/main.dart
+++ example/draw_primitive/lib/main.dart
@@ -16,31 +16,3 @@ void main() {
   runApp(game);
   game.start();
 }
-
-
-/*
-import 'package:flutter/widgets.dart';
-import 'package:flutter/rendering.dart';
-import 'package:umiuni2d/core.dart';
-import 'package:umiuni2d_flutter/core.dart';
-import 'primitive_test.dart';
-
-void main() {
-  runApp(new GameWidget());
-}
-
-class GameWidget extends SingleChildRenderObjectWidget {
-  GameWidget() {}
-
-  @override
-  RenderObject createRenderObject(BuildContext context) {
-    TinyGameBuilderForFlutter builder = new TinyGameBuilderForFlutter(assetsRoot:"web/");
-    builder.useTestCanvas = true;
-    GameRoot root = new GameRoot(400.0, 300.0);
-    Stage stage = builder.createStage(root: root);
-    stage.start();
-    stage.root.addChild(new PrimitiveTest(builder));
-    return (stage as TinyFlutterStage);
-  }
-}
-*/
